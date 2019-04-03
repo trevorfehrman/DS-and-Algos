b00abf4fb1a5eb279adf8c73f66c1e8bb6b9997d
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-02 22:14:50 -0700

    push

---
+++ Singly Linked LIsts/1.js
@@ -0,0 +1,29 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+            
+        }
+
+        this.length++;
+        return this;
+    }
+}
