9ba0baac6db119ede9ed2bcfc98d7a55dbf41288
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-08 14:15:09 -0700

    pop

--- Singly Linked LIsts/1.js
+++ Singly Linked LIsts/1.js
@@ -1,29 +1,62 @@
-class Node{
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SinglyLinkedList{
-    constructor() {
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
-    push(val) {
-        var newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-            
-        }
-
-        this.length++;
-        return this;
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
     }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.lenth === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  // A naive example
+  // traverse() {
+  //     var current = this.head;
+  //     while (current) {
+  //         console.log(current.val);
+  //         current = current.next;
+  //     }
+  // }
 }
+
+var list = new SinglyLinkedList();
+list.push('hi');
+list.push('byeeee');
+list.pop();
+list.pop();
+
+console.log(JSON.stringify(list));
