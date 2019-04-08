a7fb8faef3b6fb23b1ef0d212cbb34b770c26292
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-08 16:26:51 -0700

    remove

--- Singly Linked LIsts/1.js
+++ Singly Linked LIsts/1.js
@@ -79,25 +79,37 @@ class SinglyLinkedList {
   }
 
   set(index, val) {
-      var foundNode = get(index);
-      if(foundNode) {
-          foundNode.val = val;
-          return true;
-      }
-      return false;
+    var foundNode = get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
   }
 
   insert(index, val) {
     if (index < 0 || index > this.length) return false;
-    if (index === this.length) return this.push(val);
-    if (index === 0) return this.unshift(val)
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
     newNode = new Node(val);
-    var prev = this.get(index -1);
+    var prev = this.get(index - 1);
     var temp = prev.next;
     prev.next = newNode;
     newNode.next = temp;
     this.length++;
     return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
   }
 
   // A naive example
