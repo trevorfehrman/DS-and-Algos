edd689c3cf6705fb548eef02d49763206f4c51e2
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-08 15:58:48 -0700

    insert

--- Singly Linked LIsts/1.js
+++ Singly Linked LIsts/1.js
@@ -87,6 +87,19 @@ class SinglyLinkedList {
       return false;
   }
 
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(val);
+    if (index === 0) return this.unshift(val)
+    newNode = new Node(val);
+    var prev = this.get(index -1);
+    var temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
   // A naive example
   // traverse() {
   //     var current = this.head;
