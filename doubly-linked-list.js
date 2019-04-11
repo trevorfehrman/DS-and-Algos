a73f2f712ec7836484d4554e867fdeec95563e7a
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-10 22:38:08 -0700

    pop

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -26,4 +26,19 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!head) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
 }
