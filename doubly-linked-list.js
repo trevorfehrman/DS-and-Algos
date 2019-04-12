8816bdcd7665b7f3fbcca3f22c1142813d65e196
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-11 18:36:38 -0700

    set

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -55,4 +55,40 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      var count = 0;
+      var current = this.head;
+
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      var count = this.length - 1;
+      var current = this.tail;
+      while (count != index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
 }
