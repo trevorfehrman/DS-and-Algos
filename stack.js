a79effd8cb30e633ad4bfe3130bff1a42f514628
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-13 11:14:40 -0700

    pop

--- stack.js
+++ stack.js
@@ -24,4 +24,16 @@ class Stack {
     }
     return ++this.size;
   }
+
+  pop() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first == this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
 }
