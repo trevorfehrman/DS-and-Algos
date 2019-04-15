2a58e83083fab93ac17895185931be1f8a6fe436
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-15 15:22:46 -0700

    DFS pre and post

--- bst.js
+++ bst.js
@@ -76,4 +76,44 @@ class BST {
     }
     return data;
   }
+
+  DFSPreOrder() {
+    var data = [];
+    var current = this.root;
+
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) {
+        traverse(node.left);
+      }
+
+      if (node.right) {
+        traverse(node.right);
+      }
+    }
+
+    traverse(current);
+
+    return data;
+  }
+
+  DFSPostOrder() {
+    var data = [];
+    var current = this.root;
+
+    function traverse(node) {
+      if (node.left) {
+        traverse(node.left);
+      }
+
+      if (node.right) {
+        traverse(node.right);
+      }
+      data.push(node.value);
+    }
+
+    traverse(current);
+
+    return data;
+  }
 }
