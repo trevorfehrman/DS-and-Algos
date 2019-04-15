7e114712fcae3a1779de93d0da41dcabd82de652
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-15 14:46:05 -0700

    BFS

--- bst.js
+++ bst.js
@@ -56,4 +56,24 @@ class BST {
     if (!found) return undefined;
     return current;
   }
+
+  BFS() {
+    var data = [];
+    var queue = [];
+    var node = this.root;
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+    return data;
+  }
 }
