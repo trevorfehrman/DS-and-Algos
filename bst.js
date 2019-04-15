d42c5ee9365260f82da588cf45b9b871dbb45b7a
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-15 14:04:05 -0700

    find

--- bst.js
+++ bst.js
@@ -38,4 +38,22 @@ class BST {
       }
     }
   }
+
+  find(value) {
+    if (this.root == null) return false;
+    var current = this.root;
+    var found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
 }
